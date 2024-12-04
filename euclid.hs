6d5b3d569ac6e236be7ce52914ac5dd61f53bd04
Nestor Hiebl <git@nestorhiebl.com>
2024-12-04 17:17:23 +0100

    Include coprime test in hs file

--- euclid.hs
+++ euclid.hs
@@ -13,6 +13,15 @@ gcd a b =  gcd b' (a' `mod` b')
 
 gcdFold = foldr (gcd) 0
 
+coprimes :: Natural -> Natural -> Bool
+coprimes a b = (gcd a b) == 1
+
+-- Cardinality of {0 < i < n | gcd(i,n) == 0}
+numCoprimesOfBase :: Natural -> Natural
+numCoprimesOfBase n = foldr (inc) 0 [1..n]
+    where
+        inc m acc = if coprimes n m then acc + 1 else acc
+
 handleArgs :: [String] -> String
 handleArgs args = case length args of 
     0 -> "Usage: Provide arbitrary natural numbers to find GCD of"
